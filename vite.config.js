8444f3091eb7116e52a5c362a5fd1fdcd12b2c19
User <user@example.com>
2026-04-17 22:44:56 +0600

    Deploy app under docs with root redirect for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/docs/',
   build: {
     outDir: 'docs',
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, 'app.html'),
+      },
+    },
   },
   plugins: [react()],
 })
